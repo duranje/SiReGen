3616fcb00168f7b7f1037bfe8daff78ca0f756af
Sheharyar Naseer <hello@sheharyar.me>
2018-10-12 21:41:32 -0400

    Using Inflector to pluralize model names

--- siregen.rb
+++ siregen.rb
@@ -1,48 +1,53 @@
+require 'dry/inflector'
+
+inflector = Dry::Inflector.new
 model = ARGV[0]
+model_plural = inflector.pluralize(model)
+
 router = <<-ROUTER
-get "/#{model}s" do
-	@#{model}s = #{model.capitalize}.all
-	erb :"/#{model}s/index"
-end
+  get "/#{model_plural}" do
+    @#{model_plural} = #{model.capitalize}.all
+    erb :"/#{model_plural}/index"
+  end
 
-get "/#{model}s/new" do
-	erb :"/#{model}s/new"
-end
+  get "/#{model_plural}/new" do
+    erb :"/#{model_plural}/new"
+  end
 
-post "/#{model}s" do
-	@#{model} = #{model.capitalize}.new(params[:#{model}])
-	if @#{model}.save
-		if request.xhr?
-			@#{model}
-		else
-			redirect "/#{model}s"
-		end
-	else
-		erb :"/#{model}s/new"
-	end
-end
+  post "/#{model_plural}" do
+    @#{model} = #{model.capitalize}.new(params[:#{model}])
+    if @#{model}.save
+      if request.xhr?
+        @#{model}
+      else
+        redirect "/#{model_plural}"
+      end
+    else
+      erb :"/#{model_plural}/new"
+    end
+  end
 
-get "/#{model}s/:id" do
-	@#{model} = #{model.capitalize}.find(params[:id])
-	erb "/#{model}s/show"
-end
+  get "/#{model_plural}/:id" do
+    @#{model} = #{model.capitalize}.find(params[:id])
+    erb "/#{model_plural}/show"
+  end
 
-get "/#{model}s/:id/edit" do
-	erb :"/#{model}s/edit"
-end
+  get "/#{model_plural}/:id/edit" do
+    erb :"/#{model_plural}/edit"
+  end
 
-put "/#{model}s/:id" do
-	@#{model} = #{model.capitalize}.find(params[:id])
-	@#{model}.update(params[:#{model}])
-	redirect "/#{model}s/params[:id]"
-end
+  put "/#{model_plural}/:id" do
+    @#{model} = #{model.capitalize}.find(params[:id])
+    @#{model}.update(params[:#{model}])
+    redirect "/#{model_plural}/params[:id]"
+  end
 
-delete "/#{model}s/:id" do
-	@#{model} = #{model.capitalize}.find(params[:id])
-	@#{model}.destroy
-	redirect "/#{model}s/index"
-end
+  delete "/#{model_plural}/:id" do
+    @#{model} = #{model.capitalize}.find(params[:id])
+    @#{model}.destroy
+    redirect "/#{model_plural}/index"
+  end
 ROUTER
 
-f = File.new("#{model}s.rb", "wb")
+f = File.new("#{model_plural}.rb", "wb")
 f.write(router)
